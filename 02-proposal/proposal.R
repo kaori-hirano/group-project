d77412d5b0e653e2924dbbbf65a0efeae06f5906
kaori-hirano <hiranok@carleton.edu>
2023-06-29 23:00:38 +0000

    updates format

--- 02-proposal/proposal.R
+++ 02-proposal/proposal.R
@@ -1,6 +1,22 @@
 ---
-  title: "Project Proposal"
+title: "Project Proposal"
 author: "Group 4"
 date: "6/29/23"
 format: pdf
 ---
+  
+### Project Proposal
+
+# Question 1
+  
+  # Research Question
+  
+  # Data for Question 1
+  
+  
+## Question 2
+  
+  # Research Question
+
+  # Data for Question 2
+  
