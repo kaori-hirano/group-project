e379be4bef306deb64ee8ea89c8ae82b503d7251
James Xia <jxia@mail.wlu.edu>
2023-06-30 03:59:47 +0000

    topic 2 data

--- 02-proposal/proposal.R
+++ 02-proposal/proposal.R
@@ -19,4 +19,10 @@ format: pdf
   # Research Question
 
   # Data for Question 2
+## Since our topic is about how civil society index be affected by prosocial behaviors, we are 
+## going to use all the Freedom House scores and democracy, elections and participation indexes. 
+## In the first part of the data processing, we might see the importance of each scores and indexes among 
+## their corresponding categories. After choosing the siginificant factors, both supervised and 
+## unsupervised tranining will be conducted by linear model and clustering. 
+
   
