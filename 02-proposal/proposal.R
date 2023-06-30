5313ea42d2e2be15abd354a85c7e1ef4e6b8d476
kaori-hirano <hiranok@carleton.edu>
2023-06-30 04:31:52 +0000

    finish q2 research question

--- 02-proposal/proposal.R
+++ 02-proposal/proposal.R
@@ -16,7 +16,29 @@ format: pdf
   
 ## Question 2
   
-  # Research Question
+  ### Research Question
+Our second research question idea explores the relationship  between prosocial/social
+behaviors and civil society index. This question is of interest because it pulls
+together politics and measures of wellbeing to explore what effects civil society,
+which is the area outside of business and government, such as family and community.
+We want to explore how various measures of community engagement and support,
+both positive and negative, predict measure civil
+society scores. This will allow us to practice both clustering and regression modelling
+to answer this question. We will also be able to practice cleaning and merging data.
+While we cannot say for certain what relationships we expect to see, finding relationships
+between the democracy and happiness index datasets is an exciting possibility.
+Specific relationships we could see include a potentially negative
+between civil society organization oppression, freedom to make life choices,
+and wars/coups, a potentially positive relationship between social support,
+participation  rate in civil society, generosity, education, and maybe
+government corruption. We expect places with high civil society indexes to have 
+higher amounts of things that bring people together and lower rates of things that 
+bring people apart or make meeting with other people difficult. 
+
+Alternately, we could explore predicting civil society participation instead of 
+the index. We could also explore if these predictors of civil society also predict 
+happiness, which is said to be one of the reasons for participation in civil society. 
+
 
   # Data for Question 2
 ## Since our topic is about how civil society index be affected by prosocial behaviors, we are 
